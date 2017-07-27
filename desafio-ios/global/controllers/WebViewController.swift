e48abf5ffdfc751d9da8748b1dd539b2239877f9
Felipe Lefèvre Marino <felipemarino91@gmail.com>
2017-07-27 00:11:42 -0300

    showing activity indicator when loading url

--- desafio-ios/global/controllers/WebViewController.swift
+++ desafio-ios/global/controllers/WebViewController.swift
@@ -11,10 +11,16 @@ import UIKit
 class WebViewController: UIViewController {
     
     @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     public var url: URL?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         loadRequest()
     }
 
@@ -24,8 +30,24 @@ class WebViewController: UIViewController {
     }
     
     private func loadRequest() {
+        activityIndicator.isHidden = false
         if let _url = url {
             webView.loadRequest(URLRequest(url: _url))
         }
+    }
+}
+
+extension WebViewController: UIWebViewDelegate {
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        activityIndicator.startAnimating()
+        return true
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        activityIndicator.stopAnimating()
+    }
+    
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        print("failed request with error: \(error)")
     }
 }
